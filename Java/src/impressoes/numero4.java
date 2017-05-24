b6c6303ee2eeabf678213784e48292c8d2fbf893
Noturno <Noturno@192.168.7.22>
2017-05-23 21:57:36 -0300

    Aula dia 23/05 ..

---
+++ Java/src/impressoes/numero4.java
@@ -0,0 +1,12 @@
+package impressoes;
+
+public class numero4 {
+	public static void main(String[] args) {
+		for (int a = 0; a <= 40; a++) {
+			System.out.println(a);
+			if (a == 20) {
+				a += 9;
+			}
+		}
+	}
+}
---
+++ Java/src/impressoes/numero5.java
@@ -0,0 +1,13 @@
+package impressoes;
+
+import javax.swing.JOptionPane;
+
+public class numero5 {
+	public static void main(String[] args) {
+		int _number = Integer.parseInt(JOptionPane.showInputDialog("Digite até qual numero que deseja imprimir"));
+		for (int a = 0; a <= _number; a++) {
+			System.out.println(" Os números são " + a);
+		}
+
+	}
+}
